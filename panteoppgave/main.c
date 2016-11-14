25865e9ee6dc3733167e62367043c26bd6d35ee4
Christer Sørem <Christer@Christers-MacBook-Pro-2.local>
2016-11-14 10:37:59 +0100

    Initial Commit

---
+++ panteoppgave/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  panteoppgave
+//
+//  Created by Christer Sørem on 14.11.2016.
+//  Copyright © 2016 Christer Sørem. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
